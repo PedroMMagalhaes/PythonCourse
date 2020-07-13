c10328d477ee6223c45c271152cdd60798cd9b57
Pedro Magalhães <pedromagtaem@gmail.com>
2020-07-13 22:19:28 +0100

    Create FormattingDates.py

---
+++ Begginer/FormattingDates.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+def main():
+  # Times and dates can be formatted using a set of predefined string
+  # control codes 
+
+  now = datetime.now()
+  
+  #### Date Formatting ####
+  
+  # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
+  print(now.strftime("%a, %d, %b, %y"))
+
+  # %c - locale's date and time, %x - locale's date, %X - locale's time
+  print(now.strftime("Local date and time: %c"))
+  print(now.strftime("Local date: %x"))
+  print(now.strftime("Local time: %X"))
+
+  #### Time Formatting ####
+  
+  # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
+  print(now.strftime("Current time: %I:%M:%S %p"))
+
+
+if __name__ == "__main__":
+  main();
